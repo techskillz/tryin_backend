a711058239dda85727337124721292356017f741
techskillz <163603186+techskillz@users.noreply.github.com>
2024-09-17 19:29:41 +0100

    i cant fiure out the issue

--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,48 @@ const express = require("express");
 const router = express.Router();
 const user = require("../models/User");
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
     const savedUser = await newUser.save();
     res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// for login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+
+    !user && res.status(401).json("wrong credentials");
+
+    const hashedpassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const originalPassword = hashedpassword.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password {
+        
+    }
+      res.status(401).json("wrong credentials");
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).json(error);
   }
